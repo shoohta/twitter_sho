8be96aa29c3ffbd170e0db17bab56067b7bd2790
大田 将 <otasho@oota-no-MacBook-Air.local>
2014-11-04 22:02:30 +0900

    second

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
    def index
-    @users = User.paginate(page: params[:page], :per_page => 5)
+    @users
+     = User.paginate(page: params[:page], :per_page => 5)
   end
 
   # GET /users/1
